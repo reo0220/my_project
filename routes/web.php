8255c05c7e87eb5908d933aeeb45336dc846560d
{reo0220} <{k_reo0220@icloud.com}>
2025-02-26 22:50:15 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\IndexController::class, 'index']);
-Route::get('/top', [App\Http\Controllers\TopController::class, 'index']);
-Route::get('/sample', [App\Http\Controllers\SampleController::class, 'index']);
-
-Route::get('/login', [App\Http\Controllers\LoginController::class, 'index']);
-Route::post('/login/register', [App\Http\Controllers\LoginController::class, 'register']);
-Route::get('/login/unregister', [App\Http\Controllers\LoginController::class, 'unregister']);
-Route::post('/login/sign_in', [App\Http\Controllers\LoginController::class, 'sigin_in']);
-
-Route::get('/test', [App\Http\Controllers\TestController::class, 'index']);
